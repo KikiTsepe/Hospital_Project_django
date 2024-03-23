8bb63a56d62959630a416ba986c7057cb888520e
Kiki Tsepe <kiki.tsepe@gmail.com>
2024-03-23 12:29:58 +0200

    -Added some docstring in models. -Added a testing template for nurses list page

--- Hospital/entities/models.py
+++ Hospital/entities/models.py
@@ -9,6 +9,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # ========= Base Models =========
 class Human(Model):
+    """Is the base for all people. Does the validations too."""
     name = CharField(max_length=40, null=False, blank=False)
     age = PositiveIntegerField(null=False, validators=[MinValueValidator(0), MaxValueValidator(110)])
 
@@ -17,6 +18,7 @@ class Human(Model):
 
 
 class Employee(Human):
+    """Is the base for workers, like class Human it has validators that ensure the proper results for the workers."""
     age = PositiveIntegerField(null=False, validators=[MinValueValidator(21), MaxValueValidator(67)])
     experience = PositiveIntegerField(null=False)
     salary = DecimalField(null=False, decimal_places=2,max_digits=10)
@@ -27,6 +29,7 @@ class Employee(Human):
 
 # ========= Entity Models =========
 class Room(Model):
+    """The model for the room"""
     number = PositiveIntegerField(null=False,unique=True, validators=[MinValueValidator(0)])
     capacity = PositiveIntegerField(null=False, validators=[MinValueValidator(1), MaxValueValidator(4)])
 
@@ -39,7 +42,7 @@ class Room(Model):
 
 # TODO: validate the experience based on it's age.
 class Doctor(Employee):
-
+    """The model for the doctor entity."""
     class Meta:
         verbose_name_plural = "Doctors"
 
@@ -48,6 +51,7 @@ class Doctor(Employee):
 
 
 class Nurse(Employee):
+    """The model for the nurse entity."""
     room_in_charge=ForeignKey(Room, null=True, on_delete=SET_NULL)
 
     class Meta:
@@ -57,8 +61,11 @@ class Nurse(Employee):
         return f"Nurse: {self.name}"
 
 class Patient(Human):
+    """The model for the patient entity."""
     disease= CharField(max_length= 60, null=False, blank= False)
+    #The patient won't be deleted incase the room is deleted.
     room=ForeignKey(Room, null=False, on_delete=RESTRICT) 
+    #The patient won't be deleted incase the doctor is deleted.
     doctor=ForeignKey(Doctor, null=False, on_delete=RESTRICT)
 
     class Meta:
